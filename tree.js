95e5634f17ba8d37a93f55d7f6822c2f05adb8cc
Michele Romano <33063403+Mik317@users.noreply.github.com>
2018-03-31 00:05:34 +0200

    Update tree.js

--- tree.js
+++ tree.js
@@ -77,7 +77,7 @@
 
 	var ParticleNetwork = function(parent) {
 		this.options = {
-			velocity: 2, // the higher the faster
+			velocity: 3, // the higher the faster
 			density: 20000, // the lower the denser
 			netLineDistance: 250,
 			netLineColor: '#03536d', //modified hic
